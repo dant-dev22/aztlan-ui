40e18251d01fc5846b036191f739b399ca3eba94
DanteNovoa <dante.novoa@alumnos.udg.mx>
2024-12-20 18:12:53 -0600

    Letrero que advierte de completar registro

--- src/templates/Home.js
+++ src/templates/Home.js
@@ -10,8 +10,14 @@ const Home = () => {
   };
 
   return (
-    <div style={{ padding: "2rem", fontFamily: "Arial, sans-serif", transition: "opacity 0.5s ease-in-out", }}>
-      <h1 style={{ textAlign: "center", color:"#1C1C1C" }}>Aztlan Grappling 2025</h1>
+    <div
+      style={{
+        padding: "2rem",
+        fontFamily: "Arial, sans-serif",
+        transition: "opacity 0.5s ease-in-out",
+      }}
+    >
+      <h1 style={{ textAlign: "center", color: "#1C1C1C" }}>Aztlan Grappling 2025</h1>
       {view === "menu" && (
         <div style={{ textAlign: "center" }}>
           <button
@@ -28,23 +34,28 @@ const Home = () => {
           >
             Registrarse
           </button>
-          <button
-            style={{
-              padding: "1rem 2rem",
-              margin: "1rem",
-              backgroundColor: "#FFC107",
-              color: "white",
-              border: "none",
-              borderRadius: "4px",
-              cursor: "pointer",
-            }}
-            onClick={() => handleViewChange("uploadProof")}
-          >
-            Subir Comprobante
-          </button>
+          <div>
+            <p style={{ margin: "1rem 0", color: "#333" }}>
+              ¿No has subido tu comprobante? Completa tu registro subiendo tu comprobante aquí:
+            </p>
+            <button
+              style={{
+                padding: "1rem 2rem",
+                margin: "1rem",
+                backgroundColor: "#FFC107",
+                color: "white",
+                border: "none",
+                borderRadius: "4px",
+                cursor: "pointer",
+              }}
+              onClick={() => handleViewChange("uploadProof")}
+            >
+              Subir Comprobante
+            </button>
+          </div>
         </div>
       )}
-      {view === "register" && <Form onBack={() => handleViewChange("menu")}/>}
+      {view === "register" && <Form onBack={() => handleViewChange("menu")} />}
       {view === "uploadProof" && <UploadProof onBack={() => handleViewChange("menu")} />}
     </div>
   );
