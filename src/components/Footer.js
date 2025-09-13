eb18f20d76995c6204384131c6f7ab679ecd1549
Dante <dante.novoa@minted.com>
2025-09-13 01:31:08 -0600

    elimnando margin top

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,6 @@ const Footer = () => {
         bottom: 0,
         left: 0,
         width: "100%",
-        marginTop: "1rem",
         backgroundColor: "gray", // Fondo transparente
         color: "white", // Texto blanco para contraste
         padding: "1rem 0",
